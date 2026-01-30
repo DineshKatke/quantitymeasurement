2953843c4d402ddb3f00bc17aadf3b2ffc2733a1
DineshKatke <katkedinesh44@gmail.com>
2026-01-30 16:58:04 +0530

    Initial Commit

---
+++ quantitymeasurementapp/src/main/java/org/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,22 @@
+package org.quantitymeasurement;
+
+public class QuantityMeasurementApp {
+
+
+    public static class Feet{
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || this.getClass() != o.getClass()) return false;
+            Feet feet = (Feet) o;
+            return Double.compare(this.value, feet.value) == 0;
+        }
+
+    }
+}
---
+++ quantitymeasurementapp/src/test/java/org/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -0,0 +1,51 @@
+package org.quantitymeasurement;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class QuantityMeasurementAppTest {
+
+@Mock
+QuantityMeasurementApp.Feet quantityMeasurementApp;
+
+@Test
+    public void testFeetEqualitySameValue(){
+            QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(10);
+            QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(10);
+
+            assertTrue(feet1.equals(feet2));
+    }
+
+    @Test
+    public void testFeetEqualityDiffrentValue(){
+        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(10);
+        QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(11.11);
+
+        assertFalse(feet1.equals(feet2));
+    }
+
+    @Test
+    public void testFeetEqualityNullComparison(){
+        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(10);
+        QuantityMeasurementApp.Feet feet2 = null;
+
+        assertFalse(feet1.equals(feet2));
+    }
+
+    @Test
+    public void testFeetEqualityDiffrentClass(){
+        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(10);
+        String test = "test";
+
+        assertFalse((feet1.equals(test)));
+    }
+
+    @Test
+    public void testFeetEqualitySameReference(){
+        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(10);
+
+        assertTrue((feet1.equals(feet1)));
+    }
+}
